8fc3153bc846e0eea1b84aa7a3088c3a10478e52
zhangyule <838409996@qq.com>
2019-12-18 23:26:35 +0800

    Hello Triangle

--- FirstOpenGL/FirstOpenGL/main.cpp
+++ FirstOpenGL/FirstOpenGL/main.cpp
@@ -9,6 +9,31 @@ void processInput(GLFWwindow *window);
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
+//顶点着色器代码
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";	//千万别忘记这个\0
+
+//片元着色器代码
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\0";	//千万别忘记这个\0
+
+//顶点
+float vertices[] = {
+	-0.5f,-0.5f,0.0f,
+	0.5f,-0.5f,0.0f,
+	0.0f,1.0f,0.0f,
+	0.0f, -0.5f, 0.0f,  // left
+	0.9f, -0.5f, 0.0f,  // right
+	0.45f, 0.5f, 0.0f   // top 
+};
 
 int main()
 {
@@ -29,49 +54,82 @@ int main()
 	glfwMakeContextCurrent(window);//将窗口的上下文环境设置为当前主线程的上下文环境
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);//注册窗口回调函数
 
-	// glad: load all OpenGL function pointers  初始化glad
-	// ---------------------------------------
+	//  初始化glad
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
 
+	//创建、编译着色器程序
+	//顶点着色器
+	int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);//编译着色器
+
+	//片元着色器
+	int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	//链接着色器
+	int shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	//释放着色器
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	unsigned int VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glBindVertexArray(VAO);
+
+	// 0. 复制顶点数组到缓冲中供OpenGL使用
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// 1. 设置顶点属性指针
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+	glEnableVertexAttribArray(0);
+
 	// render loop 循环渲染
 	// -----------
 	while (!glfwWindowShouldClose(window))//检测是否关闭窗口
 	{
 		// input
-		// -----
 		processInput(window);
 
 		// render
-		// ------
 		glClearColor(1, 1, 0, 1.0f);//设置清屏颜色
 		glClear(GL_COLOR_BUFFER_BIT);//清屏
 
-		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-		// -------------------------------------------------------------------------------
+	    // 2. 当我们渲染一个物体时要使用着色器程序
+		glUseProgram(shaderProgram);
+		// 3. 绘制物体
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
 		glfwSwapBuffers(window);//将显示缓存和离屏缓存交换
 		glfwPollEvents();//检查是否有事件触发
 	}
 
-	// glfw: terminate, clearing all previously allocated GLFW resources.
-	// ------------------------------------------------------------------
+	glDeleteBuffers(1, &VBO);
+
+	
 	glfwTerminate();//终止进程
 	return 0;
 }
 
-// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
+
 void processInput(GLFWwindow *window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)//检查Esc键有没有按下
 		glfwSetWindowShouldClose(window, true);//设置窗口关闭
 }
 
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	// make sure the viewport matches the new window dimensions; note that width and 
